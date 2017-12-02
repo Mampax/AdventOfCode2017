2408b99170ffea3054326998fe986e4f2c7dec00
Mampax <przemygol96@gmail.com>
2017-12-02 13:09:10 +0100

    Day2

---
+++ AdventOfCode_2.py
@@ -0,0 +1,33 @@
+def read():
+    with open('Day2.txt') as text:
+        matrix = [[int(x) for x in ln.split()] for ln in text]
+
+    text.close
+    return matrix
+
+def calculate():
+    matrix = read()
+    su = 0
+
+    for x in xrange(len(matrix)):
+        mini = min(matrix[x])
+        maxi = max(matrix[x])
+        difference = maxi - mini
+        su+=difference
+    print su
+
+def calculate_2():
+    matrix = read()
+    su = 0
+    for x in xrange(len(matrix[0])):
+        for y in xrange (len(matrix[0])):
+            for k in xrange(len(matrix[0])):
+                if (k != y):
+                    if (matrix[x][y]%(matrix[x][k])==0):
+                        su+=(matrix[x][y]/(matrix[x][k]))
+                    if (matrix[x][k]%(matrix[x][y])==0):
+                        su+=(matrix[x][k]/matrix[x][y])
+    print su
+
+calculate()
+calculate_2()
